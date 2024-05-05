febdfc035650ad20a264c729fb4833fa9213b5b6
Clapclap-tech <116804529+Clapclap-tech@users.noreply.github.com>
2024-05-05 22:16:34 +0800

    first commit

---
+++ Music-Player/js/music-list.js
@@ -0,0 +1,27 @@
+let allMusic = [
+  {
+    name: "Eastside",
+    artist: "Benny Blanco, Halsey & Khalid",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Yummy",
+    artist: "Justin Bieber",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "I Ain't Worried",
+    artist: "One Republic",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "There's Nothing Holdin' Me Back",
+    artist: "Shawn Mendes",
+    img: "music-4",
+    src: "music-4"
+  },
+
+];
